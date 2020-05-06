9c0166ae11efc4d7063e7619df1c14a2b2cdcfa7
Anshul <samgalacestyle01@gmail.com>
2020-05-06 13:39:50 +0530

    Elementry plots

--- pandas/covid19_india_data_analytics/code/analyze_data.py
+++ pandas/covid19_india_data_analytics/code/analyze_data.py
@@ -1,6 +1,8 @@
 # analysing the source data retrieved from kaggle
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
+import seaborn as sea
 
 # setup the default file lookup location to cwd
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -9,5 +11,18 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 source_file = os.path.join(__location__).replace('code','source') + "/covid_19_india.csv"
 source_data = pd.read_csv(source_file)
 
-print(source_data.columns)
-print(source_data.describe())
+# print(source_data.columns)
+# print(source_data.describe())
+
+# plt.plot(source_data[source_data['State/UnionTerritory'] == 'Kerala']['Date'], source_data[source_data['State/UnionTerritory'] == 'Kerala']['Confirmed'], label='Cases Trend')
+# plt.legend()
+# plt.show()
+
+x_axis = source_data[source_data['State/UnionTerritory'] == 'Kerala']['Date']
+y_axis = source_data[source_data['State/UnionTerritory'] == 'Kerala']['Confirmed']
+
+sea.barplot(x_axis, y_axis)
+plt.show()
+
+# print(source_data[source_data['State/UnionTerritory'] == 'Kerala']['Date'])
+# # print(source_data['State/UnionTerritory'])
