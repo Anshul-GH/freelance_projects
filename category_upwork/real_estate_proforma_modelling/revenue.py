6f4baaaa5695d079f3b61ef12d4b712839d736c0
Anshul <samgalacestyle01@gmail.com>
2020-06-27 23:31:33 +0530

    Routine Commit

--- category_upwork/real_estate_proforma_modelling/revenue.py
+++ category_upwork/real_estate_proforma_modelling/revenue.py
@@ -2,84 +2,81 @@ import pandas as pd
 import os
 import re
 
-
-# function to procure the absolute path of the file to be read
-def get_file_path(filename):
-    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    filepath = os.path.join(__location__, filename)
-    return filepath
-
-def gen_rev_aggr(rev_data):
+def gen_data_aggr(df_rev_data):
     aggr_data = {}
 
     # number of units
-    no_of_units = rev_data['# of Units'].astype(int).sum()
+    no_of_units = df_rev_data['# of Units'].astype(int).sum()
+    # Total_Units
     aggr_data['Total_Units'] = no_of_units
-    
+    #################
+
     # avg square-feet (XLSX - SUMPRODUCT)
-    # remove the comma notation from square-feet data and convert to float
-    rev_data['SF'] = [float(str(val).replace(',','')) for val in rev_data['SF']]
-    rev_data['SF_SP'] = rev_data['SF'] * rev_data['# of Units'].astype(int)
-    square_feet = rev_data['SF_SP'].astype(float).sum() / no_of_units
-    rev_data = rev_data.drop('SF_SP', 1)
+    df_rev_data['SF_SP'] = df_rev_data['SF'] * df_rev_data['# of Units'].astype(int)
+    square_feet = df_rev_data['SF_SP'].astype(float).sum() / no_of_units
+    df_rev_data = df_rev_data.drop('SF_SP', 1)
+    # Avg_SF
     aggr_data['Avg_SF'] = square_feet
+    #################
 
     # avg rent/month (XLSX - SUMPRODUCT)
-    # remove the $ and comma notation from rent data and convert to float
-    rev_data['Rent/Month'] = [str(val).replace('$','') for val in rev_data['Rent/Month']]
-    rev_data['Rent/Month'] = [float(str(val).replace(',','')) for val in rev_data['Rent/Month']]
-    rev_data['RPM_SP'] = rev_data['Rent/Month'] * rev_data['# of Units'].astype(int)
-    rent_per_month = rev_data['RPM_SP'].sum() / no_of_units
-    rev_data = rev_data.drop('RPM_SP', 1)
+    df_rev_data['RPM_SP'] = df_rev_data['Rent/Month'] * df_rev_data['# of Units'].astype(int)
+    rent_per_month = df_rev_data['RPM_SP'].sum() / no_of_units
+    df_rev_data = df_rev_data.drop('RPM_SP', 1)
+    # Total_RPM
     aggr_data['Total_RPM'] = rent_per_month
+    #################
 
     # dollar per SF calculation (XLSX - IFERROR)
-    rev_data['Dollar_PSF'] = [xlsx_iferror_div(x, y) for x, y in zip(rev_data['Rent/Month'], rev_data['SF'])]
-
+    df_rev_data['Dollar_PSF'] = [xlsx_iferror_div(x, y) for x, y in zip(df_rev_data['Rent/Month'], df_rev_data['SF'])]
+    # Total_RPM
     aggr_data['Agg_Dollar_PSF'] = format(float(aggr_data['Total_RPM'] / aggr_data['Avg_SF']), '.2f')
-
-    # export calculated revenue data
-    # rev_file_name = r'target/rev_curr_inplace_rents_op.csv'
-    rev_fname = r'target/rev_curr_inplace_rents_op.csv'
-    rev_fpath = get_file_path(rev_fname)
-    rev_data.to_csv(rev_fpath)
-
-    # export aggregated revenue data
-    rev_fname = r'target/rev_curr_inplace_rents_agg_op.csv'
-    rev_fpath = get_file_path(rev_fname)
-    df = pd.DataFrame(aggr_data.items())
-    df.to_csv(rev_fpath)
+    #################
     
+    df_aggr_data = pd.DataFrame(aggr_data.items())
 
+    return df_rev_data, df_aggr_data
+
+
+
+# XLSX - IFERROR implementation
 def xlsx_iferror_div(a, b):
     return a / b if b else 0
 
 
-def process_rev_data(rev_fpath):
-    rev_data = pd.read_csv(rev_fpath, sep='\t')
-    gen_rev_aggr(rev_data)
-
-
-def process_post_renov_rent(renov_data_fpath):
-    rev_data = pd.read_csv(renov_data_fpath)
-    print(rev_data)
-    # gen_rev_aggr(rev_data)
-
-
 
 if __name__ == "__main__":
+    
+    ########################
+    # Current In-Place Rents
+    rev_fpath = r"/home/anshul/youtube/freelance_projects/category_upwork/real_estate_proforma_modelling/source/rev_curr_inplace_rents_inp.tsv"
+    rev_data = pd.read_csv(rev_fpath, sep='\t')
 
-    # Table 1 - Current In-Place Rents #
-    # tab seperated file - hence named with .tsv extension
-    rev_fname = r'source/rev_curr_inplace_rents_inp.tsv'
-    rev_fpath = get_file_path(rev_fname)
-    # read the source data and generate the calculated fields
-    process_rev_data(rev_fpath)
+    # calculations
+    df_rev_data, df_aggr_data = gen_data_aggr(rev_data)
+
+    # export calculated revenue data
+    rev_fpath = r"/home/anshul/youtube/freelance_projects/category_upwork/real_estate_proforma_modelling/target/rev_curr_inplace_rents_op.csv"
+    df_rev_data.to_csv(rev_fpath)
+    # export aggregated revenue data
+    rev_fpath = r"/home/anshul/youtube/freelance_projects/category_upwork/real_estate_proforma_modelling/target/rev_curr_inplace_rents_agg_op.csv"
+    df_aggr_data.to_csv(rev_fpath)
 
 
-    # Table 2 - Post-Renovation Rents #
-    renov_data_fname = r'rev_curr_inplace_rents_op.csv'
-    renov_data_fpath = get_file_path(renov_data_fname)
+    ########################
+    # Post-Renovation Rents
+    renov_fpath = r"/home/anshul/youtube/freelance_projects/category_upwork/real_estate_proforma_modelling/source/rev_curr_inplace_rents_inp.tsv"
+    renov_data = pd.read_csv(renov_fpath, sep='\t')
+    renov_premium_fpath = r"/home/anshul/youtube/freelance_projects/category_upwork/real_estate_proforma_modelling/source/renov_premium_inp.csv"
+    renov_premium_data = pd.read_csv(renov_premium_fpath)
+    renov_data['Rent/Month'] = renov_data['Rent/Month'].astype(float) + renov_premium_data['Premium'].astype(float)
+    
+    # calculations
+    df_renov_data, df_aggr_renov_data = gen_data_aggr(renov_data)    
 
-    process_post_renov_rent(renov_data_fpath)
-
+    # export calculated renov data
+    renov_fpath = r"/home/anshul/youtube/freelance_projects/category_upwork/real_estate_proforma_modelling/target/rev_post_renov_rents_op.csv"
+    df_renov_data.to_csv(renov_fpath)
+    # export aggregated renov data
+    renov_fpath = r"/home/anshul/youtube/freelance_projects/category_upwork/real_estate_proforma_modelling/target/rev_post_renov_rents_agg_op.csv"
+    df_aggr_renov_data.to_csv(renov_fpath)
---
+++ category_upwork/real_estate_proforma_modelling/revenue_complex.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import os
+import re
+
+
+# function to procure the absolute path of the file to be read
+def get_file_path(filename):
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    filepath = os.path.join(__location__, filename)
+    return filepath
+
+def gen_rev_aggr(rev_data):
+    aggr_data = {}
+
+    # number of units
+    no_of_units = rev_data['# of Units'].astype(int).sum()
+    aggr_data['Total_Units'] = no_of_units
+    
+    # avg square-feet (XLSX - SUMPRODUCT)
+    # remove the comma notation from square-feet data and convert to float
+    rev_data['SF'] = [float(str(val).replace(',','')) for val in rev_data['SF']]
+    rev_data['SF_SP'] = rev_data['SF'] * rev_data['# of Units'].astype(int)
+    square_feet = rev_data['SF_SP'].astype(float).sum() / no_of_units
+    rev_data = rev_data.drop('SF_SP', 1)
+    aggr_data['Avg_SF'] = square_feet
+
+    # avg rent/month (XLSX - SUMPRODUCT)
+    # remove the $ and comma notation from rent data and convert to float
+    rev_data['Rent/Month'] = [str(val).replace('$','') for val in rev_data['Rent/Month']]
+    rev_data['Rent/Month'] = [float(str(val).replace(',','')) for val in rev_data['Rent/Month']]
+    rev_data['RPM_SP'] = rev_data['Rent/Month'] * rev_data['# of Units'].astype(int)
+    rent_per_month = rev_data['RPM_SP'].sum() / no_of_units
+    rev_data = rev_data.drop('RPM_SP', 1)
+    aggr_data['Total_RPM'] = rent_per_month
+
+    # dollar per SF calculation (XLSX - IFERROR)
+    rev_data['Dollar_PSF'] = [xlsx_iferror_div(x, y) for x, y in zip(rev_data['Rent/Month'], rev_data['SF'])]
+
+    aggr_data['Agg_Dollar_PSF'] = format(float(aggr_data['Total_RPM'] / aggr_data['Avg_SF']), '.2f')
+
+    # post renovation rent calculation based on renovation premium information
+    renov_premium = pd.read_csv(r"/home/anshul/youtube/freelance_projects/category_upwork/real_estate_proforma_modelling/source/renov_premium_inp.csv")
+    print(renov_premium)
+
+    # export calculated revenue data
+    # rev_file_name = r'target/rev_curr_inplace_rents_op.csv'
+    rev_fname = r'target/rev_curr_inplace_rents_op.csv'
+    rev_fpath = get_file_path(rev_fname)
+    rev_data.to_csv(rev_fpath)
+
+    # export aggregated revenue data
+    rev_fname = r'target/rev_curr_inplace_rents_agg_op.csv'
+    rev_fpath = get_file_path(rev_fname)
+    df = pd.DataFrame(aggr_data.items())
+    df.to_csv(rev_fpath)
+
+    
+
+
+    
+
+def xlsx_iferror_div(a, b):
+    return a / b if b else 0
+
+
+def process_rev_data(rev_fpath):
+    rev_data = pd.read_csv(rev_fpath, sep='\t')
+    gen_rev_aggr(rev_data)
+
+
+def process_post_renov_rent(renov_data_fpath):
+    rev_data = pd.read_csv(renov_data_fpath)
+    print(rev_data)
+    # gen_rev_aggr(rev_data)
+
+
+
+if __name__ == "__main__":
+
+    # Table 1 - Current In-Place Rents #
+    # tab seperated file - hence named with .tsv extension
+    rev_fname = r'source/rev_curr_inplace_rents_inp.tsv'
+    rev_fpath = get_file_path(rev_fname)
+    # read the source data and generate the calculated fields
+    process_rev_data(rev_fpath)
+
+
+    # Table 2 - Post-Renovation Rents #
+    renov_data_fname = r'rev_curr_inplace_rents_op.csv'
+    renov_data_fpath = get_file_path(renov_data_fname)
+
+    process_post_renov_rent(renov_data_fpath)
+
