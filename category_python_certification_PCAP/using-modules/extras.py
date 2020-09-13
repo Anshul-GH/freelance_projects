44b10c9a17d03ee4af9ae9c06f592ae4d2290fbb
Anshul <samgalacestyle01@gmail.com>
2020-09-13 23:18:48 +0530

    Routine Commit

---
+++ category_python_certification_PCAP/using-modules/extras.py
@@ -0,0 +1,6 @@
+# print("Importing 'helper' from 'extras'.")
+import helper
+
+# print(f"__name__ in extras.py: {__name__}")
+
+name = 'Anshul Jain'
---
+++ category_python_certification_PCAP/using-modules/helper.py
@@ -0,0 +1,10 @@
+def extract_upper(phrase):
+    return list(filter(str.isupper, phrase))
+
+def extract_lower(phrase):
+    return list(filter(str.islower, phrase))
+
+if __name__ == "__main__":
+    print("HELLO FROM HELPERS")
+
+# print(f"__name__ in helper.py: {__name__}")
