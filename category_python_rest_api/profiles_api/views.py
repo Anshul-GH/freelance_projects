83b040cf6e0cf6675bd100af0c05e85e6d4a906a
Anshul <samgalacestyle01@gmail.com>
2021-07-21 00:59:17 +0530

    Added profiles viewset.

--- category_python_rest_api/profiles_api/views.py
+++ category_python_rest_api/profiles_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from profiles_api import serializers
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+
+from profiles_api import serializers
 from profiles_api import models
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     """Test API View."""
@@ -60,3 +63,11 @@ class HelloViewSet(viewsets.ViewSet):
         ]
 
         return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating, creating and updating profiles."""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
