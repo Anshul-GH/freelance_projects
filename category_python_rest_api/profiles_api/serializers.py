b9e3833cf1bf1a8c6b64ff3b73ccd592128553aa
Anshul <samgalacestyle01@gmail.com>
2021-07-21 15:20:03 +0530

    Added user profile feed API.

--- category_python_rest_api/profiles_api/serializers.py
+++ category_python_rest_api/profiles_api/serializers.py
@@ -37,3 +37,16 @@ class UserProfileSerializer(serializers.ModelSerializer):
             instance.set_password(password)
 
         return super().update(instance, validated_data)
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """A serializer for profile feed items."""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {
+            'user_profile': {
+                'read_only': True
+            }
+        }
